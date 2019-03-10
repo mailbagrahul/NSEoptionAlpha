c6ce15693669e248a18dced63c8705504c3cb396
Raaghul Umapathy <raaghul.u@gmail.com>
2019-03-10 16:24:24 +0530

    layout changes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Popeye
 # @Date:   2019-02-16 13:59:32
-# @Last Modified by:   Popeye
-# @Last Modified time: 2019-03-10 11:50:19
+# @Last Modified by:   Raaghul Umapathy
+# @Last Modified time: 2019-03-10 16:20:07
 
 import dash
 import dash_core_components as dcc
@@ -18,6 +18,7 @@ import flask
 from datetime import datetime as dt
 import pandas as pd
 import json
+import time
 import os
 
 # Local modules
@@ -25,19 +26,27 @@ import NSE_Options_Data as optdata
 import get_expiry
 
 
-# Initial Dash server
-server = flask.Flask(__name__)
-app = dash.Dash(name='nsealpha', server=server)
-app.title = 'Options Analytics for NSE'
-
-
 # For including custom CSS
-external_css = [
+external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
     # 'https://codepen.io/chriddyp/pen/brPBPO.css'
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'
+    "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css",
+    # "https://codepen.io/mailbagrahul/pen/jdVPzO.css"
+    # 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'
 ]
+
+
+# Initial Dash server
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
+app.title = 'Options Analytics for NSE'
+
+
+# Tabs style
+card_style = {
+    "box-shadow": "0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3)"
+}
 
 
 # setting up caching databse
@@ -78,10 +87,6 @@ def generate_option_data(symbolname, e_date):
 
 app.config['suppress_callback_exceptions'] = True
 
-# Adding custom CSS
-for css in external_css:
-    app.css.append_css({"external_url": css})
-
 
 app.layout = html.Div([
 
@@ -90,8 +95,9 @@ app.layout = html.Div([
             html.P(children='NSE Option Analysis', style={'text-align': 'center', 'font-size': '5em'})
         ]
     ),
-    html.Div([
-        html.Div([
+
+    html.Div(className="input-container", children=[
+        html.Div(className="area1", children=[
             html.Label('Symbol'),
             dcc.Dropdown(
                 id="symbol",
@@ -100,22 +106,24 @@ app.layout = html.Div([
                 value=symbols['Symbol'][0]
             ),
         ], style={'width': '250px', 'margin-right': 'auto',
-                  'margin-left': 'auto', 'text-align': 'center'}),
-    ]),
-    html.Div([
-        html.Div([
+                  'margin-left': 'auto', 'text-align': 'center'}
+        ),
+
+        html.Div(className="area2", children=[
             html.Label('Expiry'),
-            html.Div([dcc.Dropdown(id='dd_expirydate')])
-
+            dcc.Loading(id="loading-1", children=[html.Div([dcc.Dropdown(id='dd_expirydate')]
+                                                           )], type="dot")
         ], style={'width': '250px', 'margin-right': 'auto',
-                  'margin-left': 'auto', 'text-align': 'center'}),
+                  'margin-left': 'auto', 'text-align': 'center'}
+        ),
 
-        html.Div([
+        html.Div(className="area3", children=[
             html.Button(id='refresh-btn', n_clicks=0, children='Refresh')
             # [html.Img(src='/assets/refresh.png')]
         ], style={'width': '250px', 'margin-right': 'auto',
-                  'margin-left': 'auto', 'text-align': 'right', 'display': 'inline-block'}),
+                  'margin-left': 'auto', 'text-align': 'center'}),
     ]),
+
     html.Hr(),
 
     html.Div([html.Span(id='price_id'),
@@ -125,23 +133,35 @@ app.layout = html.Div([
              ),
 
 
-    html.Hr(),
+    # html.Hr(),
     html.Div([
-        dcc.Tabs(id="tabs-example", value='oi_tab', children=[
+        dcc.Tabs(id="tabs", value='oi_tab', children=[
             dcc.Tab(label='Open Interest', value='oi_tab'),
             dcc.Tab(label='Change in OI', value='oi_change_tab'),
-            dcc.Tab(label='Technical Chart', value='Chart_tab'),
+            # dcc.Tab(label='Technical Chart', value='Chart_tab'),
         ], colors={
-            "border": "white",
-            "primary": "gold",
-            "background": "#4BCFFA"
-        }
+            "primary": "white",
+            "background": "white",
+            "border": "#d2d2d2",
+        },
+            parent_style=card_style,
         ),
-        html.Div(id='tabs-content-example')
+
+        # html.Div(id='tabs-content', className='pv2')
+        html.Div(
+            children=[
+                dcc.Loading(id='tabs-content',
+                            type='graph', className='pv2')
+            ]
+        ),
     ]),
+    html.Hr(),
     dcc.Markdown('Created by [Raaghul Umapathy](https://twitter.com/rahul_sailorman)'),
+
     # hidden signal value - Storing in intermediate data for accessing different callbacks
     html.Div(id='signal', style={'display': 'none'})
+
+
 ], className='container'
 )
 
@@ -179,6 +199,7 @@ def update_expiry_value(symbolname, e_date, n_clicks):
 
     if n_clicks is not None:
         # call expensive scrape call to store
+
         bulk_data_temp = generate_option_data(symbolname, e_date)
 
         bulk_data = {'symbol': symbolname, 'expiry_date': e_date, 'option_data': bulk_data_temp[0].to_json(orient='split'), 'underlying_spot_price': bulk_data_temp[1], 'Maxpain_Strike': bulk_data_temp[2], 'PCR_Value': bulk_data_temp[3]}
@@ -187,7 +208,7 @@ def update_expiry_value(symbolname, e_date, n_clicks):
 
 
 # only for label text
-output_elements = ['price_id', 'pcr_id', 'maxpain_id', 'tabs-content-example']
+output_elements = ['price_id', 'pcr_id', 'maxpain_id', 'tabs-content']
 
 
 def create_callback(output_id):
@@ -195,10 +216,10 @@ def create_callback(output_id):
 
         datasets = json.loads(jsonified_cleaned_data)
 
-        if output_id == 'tabs-content-example':
+        if output_id == 'tabs-content':
             if selected_tab == 'oi_tab':
                 Scrp_data_upd = pd.read_json(datasets['option_data'], orient='split')
-                return html.Div([
+                return html.Div(id='loading-1', children=[
                     # html.H3('Open Interest'),
                     dcc.Graph(
                         id='total_oi_graph',
@@ -230,7 +251,7 @@ def create_callback(output_id):
                                 'layout': go.Layout(
                             # title='Open Interest',
                             xaxis=dict(title="Strike Price",
-                                       tickangle=-90,
+                                       # tickangle=-90,
                                        showline=True,
                                        # tickmode="linear",
                                        # dtick=100,
@@ -238,22 +259,30 @@ def create_callback(output_id):
                                        ),
 
 
-                            yaxis=dict(title="Open Interest", autorange=True, showgrid=False, gridwidth=0, tickmode="auto", zeroline=True, type="linear", showticklabels=True, automargin=True, tickformat="s", nticks=0,
+                            yaxis=dict(title="Open Interest", autorange=True, showgrid=False,
+                                       # gridwidth=4,
+                                       tickmode="auto", zeroline=True,
+                                       type="linear", showticklabels=True,
+                                       # automargin=True,
+                                       tickformat="s", nticks=0,
                                        ),
 
                             yaxis2=dict(
                                 title='PCR',
                                 overlaying='y',
                                 side='right',
-                                showgrid=False,
+                                showgrid=True,
+                                gridcolor='#1d1d21',
                                 rangemode='tozero'
                             ),
+                            paper_bgcolor='#000000',
+                            plot_bgcolor='#000000',
                             hovermode="closest",
-                            margin=dict(pad=0, r=80, b=80, l=80, t=40),
+                            # margin=dict(pad=0, r=80, b=80, l=80, t=40),
                             legend=dict(y=-0.3, x=0.3, orientation="h"),
                             showlegend=True,
-                            height=400,
-                            width=1000,
+                            # height=400,
+                            # width=1000,
                             # barmode='overlay'
                         )
                         }
@@ -304,12 +333,12 @@ def create_callback(output_id):
                         }
                     )
                 ])
-            elif selected_tab == 'Chart_tab':
-                return html.Div([
-                    dash_dangerously_set_inner_html.DangerouslySetInnerHTML('''
-                            <iframe height="400" width="1500" src="https://ssltvc.forexprostools.com/?pair_ID=8985&height=400&width=1500&interval=86400&plotStyle=candles&domain_ID=1&lang_ID=1&timezone_ID=20"></iframe>
-                    '''),
-                ])
+            # elif selected_tab == 'Chart_tab':
+            #     return html.Div([
+            #         dash_dangerously_set_inner_html.DangerouslySetInnerHTML('''
+            #                 <iframe height="400" width="1500" src="https://ssltvc.forexprostools.com/?pair_ID=8985&height=400&width=1500&interval=86400&plotStyle=candles&domain_ID=1&lang_ID=1&timezone_ID=20"></iframe>
+            #         '''),
+            #     ])
         elif output_id == 'price_id':
             stock_price = datasets['underlying_spot_price']
             return '''
@@ -331,7 +360,7 @@ def create_callback(output_id):
 
 for output_element in output_elements:
     dynamically_generated_function = create_callback(output_element)
-    app.callback(Output(output_element, 'children'), [Input('tabs-example', 'value'),
+    app.callback(Output(output_element, 'children'), [Input('tabs', 'value'),
                                                       Input('signal', 'children')])(dynamically_generated_function)
 
 
--- testing_Space.py
+++ testing_Space.py
@@ -1,72 +1,215 @@
 import dash
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import plotly.graph_objs as go
+import time
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+from dash.dependencies import Input, Output, State
 
-navbar_children = [
-    dbc.NavItem(dbc.NavLink("Page 1", href="/page-1")),
-    dbc.NavItem(dbc.NavLink("Page 2", href="/page-2")),
-]
+external_stylesheets = [
+    "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"]
 
-navbar = dbc.Navbar(navbar_children, sticky="top")
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# define content for page 1
-page1 = dbc.Card(
-    [
-        dbc.CardTitle("Page 1 contents"),
-        dbc.CardText("You can replace this with whatever you like"),
-    ],
-    body=True,
-)
+card_style = {
+    "box-shadow": "0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3)"
+}
 
-# define content for page 2
+app.scripts.config.serve_locally = True
 
-tab1 = dbc.Card(
-    [
-        dbc.CardTitle("Page 2, tab 1 contents"),
-        dbc.CardText("You can replace this with whatever you like"),
-    ],
-    body=True,
-)
+app.layout = dcc.Loading(
+    children=[html.Div(
+        className="sans-serif",
+        children=[
+            html.Div(
+                [
+                    html.H1(
+                        className="tc f1 hot-pink mv6", children=["Dash Loading States API"]
+                    )
+                ]
+            ),
+            html.Div(
+                className="w-60 center pt4",
+                children=[
+                    dcc.Tabs(
+                        id="tabs",
+                        value="tab-1",
+                        children=[
+                            dcc.Tab(label="Normal Graph", value="tab-1",
+                                    style={'backgroundColor': '#f5f5f5'}),
+                            dcc.Tab(label="Funky Graph", value="tab-2",
+                                    style={'backgroundColor': '#f5f5f5'}),
+                        ],
+                        colors={
+                            "primary": "white",
+                            "background": "white",
+                            "border": "#d2d2d2",
+                        },
+                        parent_style=card_style,
+                    ),
+                    html.Div(
+                        children=[
+                            dcc.Loading(id='tabs-content',
+                                        type='graph', className='pv6')
+                        ],
+                        className='pa4'
+                    ),
+                ],
+                style={},
+            ),
+            html.Div(
+                className="w-80 center",
+                children=[
+                    dcc.Loading(id='output-1', color='gold')
+                ],
+            ),
+            html.Div(
+                className="w-80 center",
+                children=[
+                    dcc.Input(
+                        className="db center mv4 ph2 pv1", id="input-1", value="Type here!"
+                    )
+                ],
+            ),
+        ],
+    )], type='cube', fullscreen=True)
 
-tab2 = dbc.Card(
-    [
-        dbc.CardTitle("Page 2, tab 1 contents"),
-        dbc.CardText("Let's write something different here for fun"),
-    ],
-    body=True,
-)
 
-page2 = dbc.Tabs(
-    [
-        dbc.Tab(tab1, label="Tab 1", className="mt-3"),
-        dbc.Tab(tab2, label="Tab 2", className="mt-3"),
-    ]
-)
-
-# define page layout
-app.layout = html.Div(
-    [
-        dcc.Location(id="url", pathname="/page-1"),
-        navbar,
-        dbc.Container(id="content", style={"padding": "20px"}),
-    ]
-)
+@app.callback(Output('output-1', 'children'), [Input('input-1', 'value')])
+def render_text(value):
+    time.sleep(2)
+    return html.H1([value], className='tc gold')
 
 
-# create callback for modifying page layout
-@app.callback(Output("content", "children"), [Input("url", "pathname")])
-def display_page(pathname):
-    if pathname == "/page-1":
-        return page1
-    if pathname == "/page-2":
-        return page2
-    # if not recognised, return 404 message
-    return html.P("404 - page not found")
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    time.sleep(2)
+    if tab == 'tab-1':
+        return html.Div(id='loading-1', children=[
+            dcc.Graph(
+                id='graph-2-tabs',
+                figure=go.Figure(
+                    data=[
+                        go.Bar(
+                            x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                               2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                            y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                               350, 430, 474, 526, 488, 537, 500, 439],
+                            name='Rest of world',
+                            marker=go.bar.Marker(
+                                color='rgb(55, 83, 109)'
+                            )
+                        ),
+                        go.Bar(
+                            x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                               2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                            y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                               299, 340, 403, 549, 499],
+                            name='China',
+                            marker=go.bar.Marker(
+                                color='rgb(26, 118, 255)'
+                            )
+                        )
+                    ],
+                    layout=go.Layout(
+                        title='US Export of Plastic Scrap',
+                        showlegend=True,
+                        legend=go.layout.Legend(
+                            x=0,
+                            y=1.0
+                        ),
+                        margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+                    )
+                ),
+            )
+        ])
+    elif tab == 'tab-2':
+        return html.Div(id='loading-2', children=[
+            dcc.Graph(
+                id='graph-1-tabs',
+                figure=go.Figure(
+                    data=[
+                        go.Bar(
+                            x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                               2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                            y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                               350, 430, 474, 526, 488, 537, 500, 439],
+                            name='Rest of world',
+                            marker=go.bar.Marker(
+                                color='hotpink'
+                            )
+                        ),
+                        go.Bar(
+                            x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                               2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                            y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                               299, 340, 403, 549, 499],
+                            name='China',
+                            marker=go.bar.Marker(
+                                color='gold'
+                            )
+                        )
+                    ],
+                    layout=go.Layout(
+                        title='US Export of Plastic Scrap',
+                        showlegend=True,
+                        legend=go.layout.Legend(
+                            x=0,
+                            y=1.0
+                        ),
+                        margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+                    )
+                ),
+            )
+        ])
 
 
 if __name__ == "__main__":
-    app.run_server(debug="True")
+    app.run_server(debug=True)
+
+
+# import dash
+# import dash_html_components as html
+# import dash_core_components as dcc
+# import plotly.graph_objs as go
+# import time
+
+# from dash.dependencies import Input, Output, State
+
+# app = dash.Dash(__name__)
+
+# app.scripts.config.serve_locally = True
+
+# app.layout = html.Div(
+#     children=[
+#         dcc.Loading(id="loading-1", children=[html.Div(id="output-1")], type="dot"),
+#         dcc.Input(id="input-1", value='Input triggers local spinner'),
+#         html.Div(
+#             [
+#                 dcc.Loading(
+#                     id="loading-2",
+#                     children=[html.Div([html.Div(id="output-2")])],
+#                     type="graph",
+#                 ),
+#                 dcc.Input(id="input-2", value='Input triggers nested spinner'),
+#             ]
+#         ),
+#     ],
+# )
+
+
+# @app.callback(Output("output-1", "children"), [Input("input-1", "value")])
+# def input_triggers_spinner(value):
+#     time.sleep(1)
+#     return value
+
+
+# @app.callback(Output("output-2", "children"), [Input("input-2", "value")])
+# def input_triggers_nested(value):
+#     time.sleep(5)
+#     return value
+
+
+# if __name__ == "__main__":
+#     app.run_server(debug=True)
