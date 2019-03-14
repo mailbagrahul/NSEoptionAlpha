ee82ef2991739930b27ec40915fde2380e600925
Raaghul Umapathy <raaghul.u@gmail.com>
2019-03-15 01:37:20 +0530

    Flask wrapper added

---
+++ server.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: Popeye
+# @Date:   2019-03-15 01:29:21
+# @Last Modified by:   Popeye
+# @Last Modified time: 2019-03-15 01:29:28
+from flask import Flask
+server = Flask(__name__)
+server.secret_key = 'jfhdjsfhdsfhsdf7678'
